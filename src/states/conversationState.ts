d85d44fa42859067bfe283741078b9ba965ded81
fuhuan1991 <huanfu2019@gmail.com>
2026-06-14 11:06:19 -0700

    Add conversationState

--- src/states/conversationState.ts
+++ src/states/conversationState.ts
@@ -1,8 +1,13 @@
-import { MessagesAnnotation, Annotation } from "@langchain/langgraph";
+import { MessagesAnnotation, Annotation, messagesStateReducer } from "@langchain/langgraph";
+import { BaseMessage } from "@langchain/core/messages";
 
 // Annotation is a field descriptor that bundles together a field's type and its reducer (the merge function).
 
 export const ConversationState = Annotation.Root({
   ...MessagesAnnotation.spec,
   research_brief: Annotation<string>({ reducer: (_, next) => next, default: () => "" }),
+  supervisor_messages: Annotation<BaseMessage[]>({ reducer: messagesStateReducer, default: () => [] }),
+  raw_notes: Annotation<string[]>({ reducer: (a, b) => [...a, ...b], default: () => [] }),
+  notes: Annotation<string[]>({ reducer: (a, b) => [...a, ...b], default: () => [] }),
+  final_report: Annotation<string>({ reducer: (_, next) => next, default: () => "" }),
 });
